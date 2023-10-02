3bf19fd28bae9737da157a20e01096dc29f07a53
IDMT-MK <107912869+IDMT-MK@users.noreply.github.com>
2023-10-02 15:09:51 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,22 @@ const text = document.getElementById('bodyText');
 //function構文を使う書き方
 function changeColor(){
   document.body.style.backgroundColor = color.value;
-  text.textContent = color.value;
+
+  //カラーコードを表示
+  if (color.value === '#ffffff') {
+    text.textContent = 'コード：' + color.value + '(white)';
+  } else if (color.value === '#000000') {
+    text.textContent = 'コード：' + color.value + '(black)';
+  } else if (color.value === '#ff0000') {
+    text.textContent = 'コード：' + color.value + '(red)';
+  } else if (color.value === '#0000ff') {
+      text.textContent = 'コード：' + color.value + '(blue)';
+  } else if (color.value === '#00ff00') {
+        text.textContent = 'コード：' + color.value + '(green)';
+  } else {
+    text.textContent = 'コード：' + color.value;
+  }
+
 }
 
 //アロー関数を使う書き方
